aac295850be8826fd6f80a0230a749158f7980b9
anwind-git <work-git@yandex.ru>
2022-10-14 14:04:32 +0300

    рекурсивный метод, который выведет все натуральные числа кратные 3-ём в промежутке от M до N.

---
+++ task_64/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значения M и N. Напишите рекурсивный метод, который 
+// выведет все натуральные числа кратные 3-ём в промежутке от M до N.
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.Write("Числа кратные 3-ём: ");
+
+void Zadaha64(int n, int m)
+{
+    m++;
+    if(m % 3 == 0)
+        Console.Write(m + ", ");
+    if(m >= n)
+        return;
+    Zadaha64(n, m);
+}
+Zadaha64(n, m);
